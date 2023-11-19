a7c34e1db48780846cbd8782e9e8c3897ec42be7
michaelact <86778470+michaelact@users.noreply.github.com>
2023-11-19 22:20:29 +0700

    init: create ecs-service autoscaling terraform submodule

---
+++ modules/ecs-service/variables.tf
@@ -0,0 +1,84 @@
+variable "cluster_name" {
+  description = "Name of ECS Cluster Name"
+  type        = string
+}
+
+variable "service_name" {
+  description = "Name of ECS Service Name"
+  type        = string
+}
+
+variable "name_prefix" {
+  description = "Prefix of the resources (cloudwatch, autoscaling policy) name"
+  type        = string
+
+  default = "ecs-service"
+}
+
+variable "auto_scaling" {
+  description = "Auto Scaling Configuration"
+  type        = object({
+    scale_in = object({
+      evaluation_period = optional(number, 5)
+      cooldown          = optional(number, 300)
+      interval_period   = optional(number, 60)
+    })
+
+    scale_out = object({
+      evaluation_period = optional(number, 3)
+      cooldown          = optional(number, 60)
+      interval_period   = optional(number, 60)
+    })
+
+    min_size          = optional(number, 1)
+    max_size          = optional(number, 3)
+  })
+
+  default = {
+    scale_in = {
+      evaluation_period = 5
+      cooldown          = 300
+      interval_period   = 60
+    }
+
+    scale_out = {
+      evaluation_period = 3
+      cooldown          = 60
+      interval_period   = 60
+    }
+
+    min_size = 1
+    max_size = 3
+  }
+}
+
+variable "metric" {
+  description = "Target Metric Configuration"
+  type        = object({
+    name           = optional(string, "MemoryUtilization")
+    statistic_type = optional(string, "Average")
+    target_high    = optional(number, 85)
+    target_low     = optional(number, 30)
+  })
+
+  default = {
+    name           = "MemoryUtilization"
+    statistic_type = "Average"
+    target_high    = 85
+    target_low     = 30
+  }
+}
+
+variable "additional_alarm_actions" {
+  description = "Additional Alarm Action to be Executed"
+  type        = list(string)
+
+  default = []
+}
+
+variable "tags" {
+  description = "Tags of Resources"
+  type        = map(string)
+
+  default = {}
+}
